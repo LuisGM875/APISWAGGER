f0d38289763bb2bcf2eeb8041ed5cc98f3751590
LuisAngelGM <luis.garduno@deinsi.com>
2025-06-26 19:15:32 -0600

    Agregar archivos de proyecto.

---
+++ APISWAGGER/Controllers/AritmeticaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APISWAGGER.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AritmeticaController : Controller
+    {
+        [HttpGet("sumar")]
+        public ActionResult<double> Sumar(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        [HttpGet("restar")]
+        public ActionResult<double> Restar(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        [HttpGet("multiplicar")]
+        public ActionResult<double> Multiplicar(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        [HttpGet("dividir")]
+        public ActionResult<double> Dividir(double num1, double num2)
+        {
+            if (num2 == 0)
+            {
+                return BadRequest("No se puede dividir por cero");
+            }
+            return num1 / num2;
+        }
+    }
+}
